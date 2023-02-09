4026b156a7ee4b93e51cbbc5c693e93266effae4
khan-mujeeb <mujeebkhan1831@gmail.com>
2023-02-09 11:19:54 +0530

    view binding added

--- app/src/main/java/com/example/ott/activity/MainActivity.kt
+++ app/src/main/java/com/example/ott/activity/MainActivity.kt
@@ -3,10 +3,22 @@ package com.example.ott.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.ott.R
+import com.example.ott.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (binding!=null) {
+            binding = null
+        }
     }
 }
