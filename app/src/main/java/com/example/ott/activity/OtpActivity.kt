ea2e5dc39ae2b814b40958fdb9749df6ea29dc60
khan-mujeeb <mujeebkhan1831@gmail.com>
2023-02-18 23:46:07 +0530

    email validation added

--- app/src/main/java/com/example/ott/activity/OtpActivity.kt
+++ app/src/main/java/com/example/ott/activity/OtpActivity.kt
@@ -1,20 +1,25 @@
 package com.example.ott.activity
 
+import android.content.res.ColorStateList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import androidx.core.content.ContextCompat
 import com.example.ott.R
 import com.example.ott.databinding.ActivityOtpBinding
 
 class OtpActivity : AppCompatActivity() {
     private var binding: ActivityOtpBinding? = null
+    var countVal = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityOtpBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
 
         setUpOtpInput()
+
+
     }
 
     private fun setUpOtpInput() {
@@ -24,11 +29,19 @@ class OtpActivity : AppCompatActivity() {
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                countVal++
                 binding!!.two.requestFocus()
             }
 
             override fun afterTextChanged(s: Editable?) {
-
+                if (countVal==4) {
+                    binding!!.verify.backgroundTintList = ColorStateList.valueOf(
+                        ContextCompat.getColor(
+                            this@OtpActivity,
+                            R.color.btn_red
+                        )
+                    )
+                }
             }
 
         })
@@ -38,11 +51,19 @@ class OtpActivity : AppCompatActivity() {
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                countVal++
                 binding!!.three.requestFocus()
             }
 
             override fun afterTextChanged(s: Editable?) {
-
+                if (countVal==4) {
+                    binding!!.verify.backgroundTintList = ColorStateList.valueOf(
+                        ContextCompat.getColor(
+                            this@OtpActivity,
+                            R.color.btn_red
+                        )
+                    )
+                }
             }
 
         })
@@ -52,14 +73,46 @@ class OtpActivity : AppCompatActivity() {
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                countVal++
                 binding!!.four.requestFocus()
             }
 
             override fun afterTextChanged(s: Editable?) {
-
+                if (countVal==4) {
+                    binding!!.verify.backgroundTintList = ColorStateList.valueOf(
+                        ContextCompat.getColor(
+                            this@OtpActivity,
+                            R.color.btn_red
+                        )
+                    )
+                }
             }
 
         })
+
+        binding!!.four.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                countVal++
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (countVal==4) {
+                    binding!!.verify.backgroundTintList = ColorStateList.valueOf(
+                        ContextCompat.getColor(
+                            this@OtpActivity,
+                            R.color.btn_red
+                        )
+                    )
+                }
+            }
+
+        })
+
+
 
     }
 }
--- app/src/main/java/com/example/ott/activity/SignupActivity.kt
+++ app/src/main/java/com/example/ott/activity/SignupActivity.kt
@@ -1,10 +1,22 @@
 package com.example.ott.activity
 
+import android.R.attr.editable
+import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.ColorStateList
+import android.graphics.Color.red
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.ott.R
 import com.example.ott.databinding.ActivitySignupBinding
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
 
 class SignupActivity : AppCompatActivity() {
     private var binding: ActivitySignupBinding? = null
@@ -13,6 +25,38 @@ class SignupActivity : AppCompatActivity() {
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
 
+        invalidateEmail()
+
+    }
+
+    private fun invalidateEmail() {
+        binding!!.email.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+            @SuppressLint("UseCompatLoadingForDrawables")
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+".toRegex()
+                val email = binding!!.email.text.toString()
+
+                if (!email.matches(emailPattern)) {
+                    binding!!.email.error = "Invalid email address"
+                } else {
+                    binding!!.verify.backgroundTintList = ColorStateList.valueOf(
+                        ContextCompat.getColor(
+                            this@SignupActivity,
+                            R.color.btn_red
+                        )
+                    )
+                    goToOtp()
+                    binding!!.email.error = null
+                }
+            }
+        })
+    }
+
+    private fun goToOtp() {
         binding!!.verify.setOnClickListener {
             startActivity(Intent(this, OtpActivity::class.java))
             finish()
