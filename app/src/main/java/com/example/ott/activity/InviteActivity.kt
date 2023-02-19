5b3726ed9cb4ab67cb9937d6e286976ebd44217f
himanshukanojia <202051085@iiitvadodara.ac.in>
2023-02-19 18:15:06 +0530

    update aspect ratios

--- app/src/main/java/com/example/ott/activity/InviteActivity.kt
+++ app/src/main/java/com/example/ott/activity/InviteActivity.kt
@@ -1,12 +1,14 @@
 package com.example.ott.activity
 
 import android.content.Intent
+import android.content.res.ColorStateList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
+import androidx.core.content.ContextCompat
 import androidx.core.widget.addTextChangedListener
 import com.example.ott.R
 import com.example.ott.databinding.ActivityInviteBinding
@@ -17,19 +19,38 @@ class InviteActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityInviteBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
-        binding!!.email.addTextChangedListener(object :TextWatcher{
+        invalidateEmail()
+    }
+    private fun invalidateEmail() {
+        binding!!.email.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
             }
 
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                binding!!.invite.isEnabled =true;
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+".toRegex()
+                val email = binding!!.email.text.toString()
+
+                if (!email.matches(emailPattern)) {
+                    binding!!.email.error = "oops! Enter Correct Email!"
+                } else {
+                    binding!!.invite.backgroundTintList = ColorStateList.valueOf(
+                        ContextCompat.getColor(
+                            this@InviteActivity,
+                            R.color.btn_red
+                        )
+                    )
+                    goToNext()
+                    binding!!.email.error = null
+                }
             }
 
             override fun afterTextChanged(p0: Editable?) {
 
             }
         })
+    }
+    private fun goToNext() {
         binding!!.invite.setOnClickListener {
             startActivity(Intent(this, NextActivity::class.java))
             finish()
