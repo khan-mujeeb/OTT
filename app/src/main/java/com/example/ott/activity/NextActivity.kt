b7e989a0c189b52eadd03aa989bcb94e52175288
himanshukanojia <202051085@iiitvadodara.ac.in>
2023-02-22 09:49:26 +0530

    nextactivity

--- app/src/main/java/com/example/ott/activity/NextActivity.kt
+++ app/src/main/java/com/example/ott/activity/NextActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.ott.R
 import com.example.ott.databinding.ActivityNextBinding
+import com.example.ott.ui.HomeFragment
 
 class NextActivity : AppCompatActivity() {
     private var binding: ActivityNextBinding? = null
@@ -13,7 +14,7 @@ class NextActivity : AppCompatActivity() {
         binding = ActivityNextBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
         binding!!.next.setOnClickListener {
-            val intent = Intent(this, UserNameActivity::class.java)
+            val intent = Intent(this,MainActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
             startActivity(intent)
 
